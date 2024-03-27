f962251f6e48f40bf925285e77b13f65a7ff0ba1
Jorjai <jorge.are.fdz@gmail.com>
2024-03-27 15:49:36 +0100

    Arreglar máscaras de los bits de control y añadir comentarios
    Se han añadido/modificado comentarios que facilitan la comprensión del código.
    
    InhibirIntTeclado() / HabilitarIntTeclado():
    Ahora se activan/desactivan las interrupciones del teclado desde IE en vez de activar/desactivar las interrupciones de cada tecla desde TECLAS_CNT.
    
    PararTempo():
    Se ha corregido un valor mal transformado a hexadecimal (0x7FBF -> 0xFFBF)

--- source/perifericos.c
+++ source/perifericos.c
@@ -26,7 +26,6 @@ int TeclaPulsada()
 	// definiciones.h:  A=0;B=1;SELECT=2;START=3;IZQUIERDA=4;DERECHA=5;
 	// ARRIBA=6;ABAJO=7;R=8;L=9;
 	
-	// Comprobar A
 	if ( (~TECLAS_DAT & 0x0001)!=0) return A;
 	if ( (~TECLAS_DAT & 0x0002)!=0) return B;
 	if ( (~TECLAS_DAT & 0x0004)!=0) return SELECT;
@@ -44,25 +43,23 @@ int TeclaPulsada()
 
 void ConfigurarTeclado(int Conf_Tec)
 {
-	TECLAS_CNT = TECLAS_CNT | Conf_Tec;
 	// Configuración del teclado. Modificar su registro de control en base a los bits
-        // activados en el parametro Conf_Tec	
+        // activados en el parametro Conf_Tec
+	
+	// Los bits con valor 1 en el parámetro Conf_Tec son los que deben tomar el valor 1 en el registro TECLAS_CNT
+	TECLAS_CNT |= Conf_Tec;
 }
 
 void ConfigurarTemporizador(int Latch, int Conf_Tempo)
 {
-	
 	// Configuración del temporizador. El latch es el valor del registro de datos del temporizador
         // Activar los bits del registro de control necesarios en base a los bits activados en el parámetro Conf_Tempo
 	
-	/*
-	El parámetro Latch es el valor que debe asignarse al registro TIMER0_DAT.
-	Los bits con valor 1 en el parámetro Conf_Tempo son los que deben tomar	el valor 1 en el registro TIMER0_CNT
-	*/
-	
+	// El parámetro Latch es el valor que debe asignarse al registro TIMER0_DAT.
 	TIMER0_DAT = Latch;
+	
+	// Los bits con valor 1 en el parámetro Conf_Tempo son los que deben tomar el valor 1 en el registro TIMER0_CNT
 	TIMER0_CNT |= Conf_Tempo;
-
 }
 
 void HabilitarIntTeclado()
@@ -70,69 +67,64 @@ void HabilitarIntTeclado()
 	// Habilitar las interrupciones del teclado
 	// Para realizar esa operación, primero deshabilitar todas las interrupciones de forma general, realizar la operación, 
 	// y después volver a habilitar las interrupciones de forma general 
+	
 	IME=0;
-        // ESCRIBID AQUÍ VUESTRO CÓDIGO
-		TECLAS_CNT |= 0x21FF;
-		//Las teclas activadas son: 010 0001 1111 1111 = 21FF (del 1 al 9 y 14)
+        
+	// Poner a 1 el bit 12 de IE
+        TECLAS_CNT |= 0x1000; // 0001000000000000
+        
 	IME=1;
 }
 
 void InhibirIntTeclado()
 {
-
 	// Deshabilitar las interrupciones del teclado
 	// Para realizar esa operación, primero deshabilitar todas las interrupciones de forma general, realizar la operación, 
 	// y después volver a habilitar las interrupciones de forma general 
 
 	IME=0;
-	// ESCRIBID AQUÍ VUESTRO CÓDIGO
-	TECLAS_CNT &= 0x5E00;
+	
+	// Poner a 0 el bit 12 de IE
+	TECLAS_CNT &= 0xEFFF; // 1110111111111111
+	
 	IME=1;
 }  
 
 void HabilitarIntTempo()
 {
-
-
 	// Habilitar las interrupciones del temporizador (timer0)
 	// Para realizar esa operación, primero deshabilitar todas las interrupciones de forma general, realizar la operación, 
 	// y después volver a habilitar las interrupciones de forma general 
 	IME=0;
-	// ESCRIBID AQUÍ VUESTRO CÓDIGO
-	/*
-	IE <- IE or 0000000000001000
-	*/
-	IE |= 0x0008;
+	
+	// Poner a 1 el bit 3 de IE
+	IE |= 0x0008; // 0000000000001000
 	
 	IME=1;
 }
 
 void InhibirIntTempo()
 {
-
 	// Deshabilitar las interrupciones del temporizador (timer0)
 	// Para realizar esa operación, primero deshabilitar todas las interrupciones de forma general, realizar la operación, 
-	// y después volver a habilitar las interrupciones de forma general 
+	// y después volver a habilitar las interrupciones de forma general
+	
 	IME=0;
 	
-	/*
-	IE <- IE AND 1111111111110111
-	*/
-	IE &= 0xFFF7;
-	
+	// Poner a 0 el bit 3 de IE
+	IE &= 0xFFF7; // 1111111111110111
 	
 	IME=1;
-
 }
 
 void PonerEnMarchaTempo()
 {
-	// ESCRIBID AQUÍ VUESTRO CÓDIGO
-	TIMER0_CNT |= 0x0040;
+	// Poner a 1 el bit 7 de TIMER0_CNT
+	TIMER0_CNT |= 0x0040; // 0000000001000000
 }
 
 void PararTempo()
 {
-	// ESCRIBID AQUÍ VUESTRO CÓDIGO
-	TIMER0_CNT &= 0x7FBF;
+	// Poner a 0 el bit 7 de TIMER0_CNT
+	TIMER0_CNT &= 0xFFBF; // 1111111110111111
 }
